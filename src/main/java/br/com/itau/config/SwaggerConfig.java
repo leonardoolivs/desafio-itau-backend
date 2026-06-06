708928acd8c6ba578e186d9afd991ee7aaeeb6b5
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-06 14:39:23 -0300

    feat: Adiciona configuração do Swagger para o cabeçalho da documentação

---
+++ src/main/java/br/com/itau/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package br.com.itau.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API para Validação de Senha")
+                        .description("API REST para validação de senhas de acordo com regras de segurança.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Leonardo Oliveira")
+                                .email("leonardo.sousaoliver@gmail.com")
+                                .url("https://github.com/leonardoolivs"))
+                );
+    }
+
+}
