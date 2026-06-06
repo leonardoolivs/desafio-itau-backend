7b577e959ce33be6c2ef716667a6a4ff80234bef
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-06 13:50:29 -0300

    build: Cria o Projeto com Lombok, Spring Web e Validator

---
+++ src/main/java/br/com/itau/DesafioItauBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioItauBackendApplication.class, args);
+    }
+
+}
