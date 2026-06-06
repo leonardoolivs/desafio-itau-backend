12f0444f897fa93f243d1a57b6125c26cd6620e2
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-06 14:39:54 -0300

    feat: Adiciona documentação de SenhaController no Swagger

--- src/main/java/br/com/itau/controllers/SenhaController.java
+++ src/main/java/br/com/itau/controllers/SenhaController.java
@@ -2,6 +2,10 @@ package br.com.itau.controllers;
 
 import br.com.itau.models.SenhaRequest;
 import br.com.itau.services.SenhaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/validar-senha")
+@RequestMapping("/validar")
+@Tag(name = "Senha")
 public class SenhaController {
 
     private final SenhaService service;
 
     @PostMapping
-    public ResponseEntity<Boolean> isValid(@RequestBody @Valid SenhaRequest dto){
-
-        Boolean senhaValida = service.isValid(dto.senha());
+    @Operation(summary = "Validar senha", description = "Retorna 'true' caso a senha atenda todos os critérios e 'false' caso contrário.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Senha validada com sucesso")
+    })
+    public ResponseEntity<Boolean> isValid(@RequestBody @Valid SenhaRequest request){
+        Boolean senhaValida = service.isValid(request.senha());
 
         return ResponseEntity.ok(senhaValida);
     }
